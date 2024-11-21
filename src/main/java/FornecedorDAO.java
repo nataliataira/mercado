2b625671e688388f61895bab7be28ad556cb62a4
Natalia Taira <nataliagtaira@gmail.com>
2024-11-21 17:11:47 -0300

    Criação dos métodos de crud na classe FornecedorDAO

--- src/main/java/FornecedorDAO.java
+++ src/main/java/FornecedorDAO.java
@@ -1,2 +1,20 @@
+import java.sql.SQLException;
+
 public class FornecedorDAO {
+
+    public void inserirFornecedor(Fornecedor fornecedor) throws SQLException {
+        //create
+    }
+
+    public void buscarFornecedor(Fornecedor fornecedor) throws SQLException {
+        //read
+    }
+
+    public void atualizarFornecedor(Fornecedor fornecedor) throws SQLException {
+        //update
+    }
+
+    public void excluirFornecedor(Fornecedor fornecedor) throws SQLException {
+        //delete
+    }
 }
