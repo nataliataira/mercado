078af6db0c5c9aa242ecc3a7d32294ac7494481a
Natalia Taira <nataliagtaira@gmail.com>
2024-11-21 17:02:54 -0300

    Criação de uma variável para o nome do banco de dados na classe ConexaoMySQL

--- src/main/java/ConexaoMySQL.java
+++ src/main/java/ConexaoMySQL.java
@@ -3,9 +3,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoMySQL {
-    private static final String URL = "jdbc:mysql://localhost:3306/superMercado";
-    private static final String USUARIO = "seu_usuario";
-    private static final String SENHA = "sua_senha";
+    private static final String NOME_BD = "mercadinho";
+    private static final String URL = "jdbc:mysql://localhost:3306/"+NOME_BD;
+    private static final String USUARIO = "root";
+    private static final String SENHA = "";
 
     public static Connection getConexao() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
