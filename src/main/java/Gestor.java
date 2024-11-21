6911f1f474e8feedf30c463b41480301daf87edd
Natalia Taira <nataliagtaira@gmail.com>
2024-11-21 16:23:57 -0300

    Atualização dos métodos de relatório e de realizarCompra na classe Gestor

--- src/main/java/Gestor.java
+++ src/main/java/Gestor.java
@@ -1,47 +1,80 @@
 import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
 
 public class Gestor extends Funcionario {
-    private List<Compra> compras;
+    private List<Compra> historicoCompras;
 
     public Gestor(int codigo, String nome, String CNPJ, String endereco, String telefone, String email) {
         super(codigo, nome, CNPJ, endereco, telefone, email);
     }
 
-    public List<Compra> getCompras() {
-        return compras;
+    public List<Compra> getHistoricoCompras() {
+        return historicoCompras;
     }
 
-    public void setCompras(List<Compra> compras) throws IllegalArgumentException {
+    public void setHistoricoCompras(List<Compra> compras) throws IllegalArgumentException {
         if (compras == null)
             throw new IllegalArgumentException("A lista de compras não pode estar vazia.");
-
-        this.compras = compras;
+        this.historicoCompras = compras;
     }
 
-    public void gerarRelatorioVendas() {
-
-    }
-
-    public void gerarRelatorioEstoque() {
-
-    }
-
-    public void gerargerarRelatorioCompras() {
-
-    }
-
-    public void gerarRelatorioDesempenhoProdutos() {
-
-    }
-
-    public void gerarRelatorioFluxoCaixa() {
-
-    }
-
-    public void realizarCompra(List<Item> itensCompra)
+    public void realizarCompra(Fornecedor fornecedor, List<Item> itens, String metodoPagamento)
     {
-        Compra compra = new Compra(itensCompra, new Date());
+        Compra compra = new Compra(fornecedor, itens, new Date(), metodoPagamento);
+    }
+
+    public void gerarRelatorioVendas(List<String> vendasView) {
+        System.out.println("=== Relatório de Vendas ===");
+        if (vendasView == null || vendasView.isEmpty())
+            System.out.println("Nenhum dado encontrado para o relatório de estoque.");
+        else {
+            System.out.println("Vendas registradas:");
+            for (String venda : vendasView)
+                System.out.println(venda);
+        }
+    }
+
+    public void gerarRelatorioEstoque(List<String> estoqueView) {
+        System.out.println("=== Relatório de Estoque ===");
+        if (estoqueView == null || estoqueView.isEmpty())
+            System.out.println("Nenhum dado encontrado para o relatório de estoque.");
+        else {
+            System.out.println("Estoque:");
+            for (String produtoEstoque : estoqueView)
+                System.out.println(produtoEstoque);
+        }
+    }
+
+    public void gerarRelatorioCompras(List<String> comprasView) {
+        System.out.println("=== Relatório de Compras ===");
+        if (comprasView == null || comprasView.isEmpty())
+            System.out.println("Nenhum dado encontrado para o relatório de compras.");
+        else {
+            System.out.println("Compras registradas:");
+            for (String compras : comprasView)
+                System.out.println(compras);
+        }
+    }
+
+    public void gerarRelatorioDesempenhoProdutos(List<String> desempenhoView) {
+        System.out.println("=== Relatório de Desempenho ===");
+        if (desempenhoView == null || desempenhoView.isEmpty())
+            System.out.println("Nenhum dado encontrado para o relatório de desempenho de produtos.");
+        else {
+            System.out.println("Desempenho:");
+            for (String prodDesempenho : desempenhoView)
+                System.out.println(prodDesempenho);
+        }
+    }
+
+    public void gerarRelatorioFluxoCaixa(List<String> fluxoCaixaView) {
+        System.out.println("=== Relatório de Fluxo de Caixa ===");
+        if (fluxoCaixaView == null || fluxoCaixaView.isEmpty())
+            System.out.println("Nenhum dado encontrado para o relatório de fluxo de caixa.");
+        else {
+            System.out.println("Fluxo de Caixa:");
+            for (String fluxoCaixa : fluxoCaixaView)
+                System.out.println(fluxoCaixa);
+        }
     }
 }
