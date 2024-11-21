fd2e47c034566ba169c112a1f0a445de3f1170c0
Natalia Taira <nataliagtaira@gmail.com>
2024-11-21 16:56:18 -0300

    Implementação de realizarVenda na classe OperadorDeCaixa

--- src/main/java/OperadorDeCaixa.java
+++ src/main/java/OperadorDeCaixa.java
@@ -1,12 +1,31 @@
+import java.util.Date;
+import java.util.List;
+
 public class OperadorDeCaixa extends Funcionario {
-    private Venda venda;
+    private List<Venda> historicoVendas;
 
     public OperadorDeCaixa(int codigo, String nome, String CNPJ, String endereco, String telefone, String email) {
         super(codigo, nome, CNPJ, endereco, telefone, email);
     }
 
-    public void realizarVenda(Venda venda)
-    {
+    public List<Venda> getHistoricoVendas() {
+        return historicoVendas;
+    }
 
+    public void setHistoricoVendas(List<Venda> historicoVendas) {
+        this.historicoVendas = historicoVendas;
+    }
+
+    public void realizarVenda(List<Item> itens, String metodoPagamento)
+    {
+        double valorTotal;
+        Venda venda = new Venda(itens, new Date(), metodoPagamento);
+        valorTotal = venda.totalOperacao();
+        System.out.println("Venda realizada com sucesso!");
+        System.out.println("Itens comprados: ");
+        for (Item item : itens) {
+            System.out.println("- " + item.getProduto().getNome() + ": " + item.getQuantidade() + " unidades.");
+        }
+        System.out.println("Valor total da compra: R$" + valorTotal);
     }
 }
