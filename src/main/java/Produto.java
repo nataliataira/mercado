74913b0dbb47c16024c57b8dbbcfefc405cd89d3
Natalia Taira <nataliagtaira@gmail.com>
2024-11-21 15:09:27 -0300

    Criação do método de alteração da quantidade de estoque na classe Produto

--- src/main/java/Produto.java
+++ src/main/java/Produto.java
@@ -1,4 +1,5 @@
 public class Produto {
+    private int codigo;
     private String codigoBarras;
     private String nome;
     private String categoria;
@@ -16,6 +17,14 @@ public class Produto {
         setFornecedor(fornecedor);
     }
 
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
     public String getCodigoBarras() {
         return codigoBarras;
     }
@@ -23,7 +32,6 @@ public class Produto {
     public void setCodigoBarras(String codigoBarras) throws IllegalArgumentException {
         if (codigoBarras == null)
             throw new IllegalArgumentException("O código de barras deve ser preenchido.");
-
         this.codigoBarras = codigoBarras;
     }
 
@@ -34,7 +42,6 @@ public class Produto {
     public void setNome(String nome) throws IllegalArgumentException {
         if (nome == null)
             throw new IllegalArgumentException("O nome do produto não pode estar vazio.");
-
         this.nome = nome;
     }
 
@@ -45,7 +52,6 @@ public class Produto {
     public void setCategoria(String categoria) throws IllegalArgumentException {
         if (categoria == null)
             throw new IllegalArgumentException("A categoria do produto não pode estar vazia.");
-
         this.categoria = categoria;
     }
 
@@ -56,7 +62,6 @@ public class Produto {
     public void setDescricao(String descricao) throws IllegalArgumentException {
         if (descricao == null)
             throw new IllegalArgumentException("A descricao do produto não pode estar vazia.");
-
         this.descricao = descricao;
     }
 
@@ -67,7 +72,6 @@ public class Produto {
     public void setFornecedor(Fornecedor fornecedor) throws IllegalArgumentException {
         if (fornecedor == null)
             throw new IllegalArgumentException("O fornecedor do produto deve ser preechido.");
-
         this.fornecedor = fornecedor;
     }
 
@@ -75,7 +79,9 @@ public class Produto {
         return quantidadeEstoque;
     }
 
-    public void setQuantidadeEstoque(int quantidadeEstoque) {
+    public void setQuantidadeEstoque(int quantidadeEstoque) throws IllegalArgumentException{
+        if (quantidadeEstoque < 0)
+            throw new IllegalArgumentException("A quantidade não pode ser negativa");
         this.quantidadeEstoque = quantidadeEstoque;
     }
 
@@ -84,6 +90,8 @@ public class Produto {
     }
 
     public void setNivelMinEstoque(int nivelMinEstoque) {
+        if (nivelMinEstoque < 1)
+            throw new IllegalArgumentException("O nível mínimo deve ser maior que 1");
         this.nivelMinEstoque = nivelMinEstoque;
     }
 
@@ -92,10 +100,17 @@ public class Produto {
     }
 
     public void setNivelMaxEstoque(int nivelMaxEstoque) {
+        if (nivelMaxEstoque < 0)
+            throw new IllegalArgumentException("O nível máximo não pode ser negativo");
         this.nivelMaxEstoque = nivelMaxEstoque;
     }
 
-    public void verificarNivelEstoque() {
-        //
+    public void atualizaQuantidade(int quantidadeAlteracao) throws Exception
+    {
+        int novaQuantidade = getQuantidadeEstoque() + quantidadeAlteracao;
+        if (novaQuantidade < 1)
+            throw new Exception("Não é possível reduzir a quantidade abaixo de zero.");
+        setQuantidadeEstoque(novaQuantidade);
+        System.out.println("Quantidade atualizada! Nova quantidade: " + getQuantidadeEstoque());
     }
 }
