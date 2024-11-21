1157471ae82a32386208b2ce6b7dde7d137929f0
Natalia Taira <nataliagtaira@gmail.com>
2024-11-21 17:18:22 -0300

    Inicio da implementação dos métodos da classe FornecedorDAO

--- src/main/java/FornecedorDAO.java
+++ src/main/java/FornecedorDAO.java
@@ -1,20 +1,38 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class FornecedorDAO {
 
     public void inserirFornecedor(Fornecedor fornecedor) throws SQLException {
-        //create
+        String sql = "inser into fornecedor (codigo, nome, cnpj, endereco, telefone, email) values (?, ?, ?, ?, ?, ?)";
+        try (Connection conexao = ConexaoMySQL.getConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
+
+        }
     }
 
     public void buscarFornecedor(Fornecedor fornecedor) throws SQLException {
-        //read
+        String sql = "select * from fornecedor where codigo = ?";
+        try (Connection conexao = ConexaoMySQL.getConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
+
+        }
     }
 
     public void atualizarFornecedor(Fornecedor fornecedor) throws SQLException {
-        //update
+        String sql = "update fornecedor set nome = ?, cnpj = ?, endereco = ?, telefone = ?, email = ? where codigo = ?";
+        try (Connection conexao = ConexaoMySQL.getConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
+
+        }
     }
 
     public void excluirFornecedor(Fornecedor fornecedor) throws SQLException {
-        //delete
+        String sql = "delete from fornecedor where codigo = ?";
+        try (Connection conexao = ConexaoMySQL.getConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
+
+        }
     }
 }
